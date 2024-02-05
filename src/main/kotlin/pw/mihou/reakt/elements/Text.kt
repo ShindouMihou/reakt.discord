367704215a90b074d1595da1fd3e05183c2bfcd5
Shindou Mihou <git@mihou.pw>
2024-02-05 11:06:42 +0800

    feat: auto-default to spaced in text

--- src/main/kotlin/pw/mihou/reakt/elements/Text.kt
+++ src/main/kotlin/pw/mihou/reakt/elements/Text.kt
@@ -4,7 +4,7 @@ import pw.mihou.reakt.Reakt
 import pw.mihou.reakt.styles.BodyConstructor
 
 typealias ReaktTextConstructor = BodyConstructor.() -> Unit
-fun Reakt.Document.Text(spaced: Boolean = false, constructor: ReaktTextConstructor) = component("pw.mihou.reakt.elements.Text") {
+fun Reakt.Document.Text(spaced: Boolean = true, constructor: ReaktTextConstructor) = component("pw.mihou.reakt.elements.Text") {
     @Suppress("NAME_SHADOWING") val constructor =  ensureProp<ReaktTextConstructor>("constructor")
     render {
         // @native directly injects a text element into the stack.
